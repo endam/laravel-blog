2693218f4a5c045ad49ee302713f3a058ede13a7
endam <yoshidam2@isao.co.jp>
2019-06-07 17:30:38 +0900

    change display format from card list to table

--- resources/views/posts/_list.blade.php
+++ resources/views/posts/_list.blade.php
@@ -1,6 +1,18 @@
-<div class="card-columns">
-    @each('posts/_show', $posts, 'post', 'posts/_empty')
-</div>
+<table class="table">
+    <thead>
+        <tr>
+            <th scope="col">title</th>
+            <th scope="col">author</th>
+            <th scope="col">posted_at</th>
+            <th scope="col">comments_count</th>
+            <th scope="col">likes_count</th>
+            <th scope="col"></th>
+        </tr>
+    </thead>
+    <tbody>
+        @each('posts/_show', $posts, 'post', 'posts/_empty')
+    </tbody>
+</table>
 
 <div class="d-flex justify-content-center">
     {{ $posts->links() }}
